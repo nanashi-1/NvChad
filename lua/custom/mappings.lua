4e774301e3c921ea9c68de157a1a6da41f38c701
nanashi-1 <bagaresalken@gmail.com>
2023-12-01 19:18:46 +0000

    change mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -11,19 +11,11 @@ M.disabled = {
 M.abc = {
   n = {
       ["<leader>pf"] = {":Telescope find_files <CR>", "Telescope Files"},
-      ["<C-w>"] = {":w <CR>", "Close"},
+      ["<C-w>"] = {":q <CR>", "Close"},
       ["<leader>["] = {":bp <CR>", "Previous Buffer"},
       ["<leader>]"] = {":bn <CR>", "Next Buffer"},
       ["<leader>bd"] = {":bd <CR>:bp <CR>", "Close Buffer"},
-      ["<leader>py"] = {"\"+y"},
-      ["<leader>pp"] = {"\"+p"},
-      ["<leader>pP"] = {"\"+P"},
       ["<leader>pg"] = {":LazyGit <CR>", "LazyGit"}
-  },
-  v = {
-      ["<leader>py"] = {"\"+y"},
-      ["<leader>pp"] = {"\"+p"},
-      ["<leader>pP"] = {"\"+P"}
   }
 }
 
